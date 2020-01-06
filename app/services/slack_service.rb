95533921c8d0abfc2af9d1606054d1cb3c825a38
Malcolm Durling <malcolm@durling.co.nz>
2020-01-07 12:29:52 +1300

    Debugging slack notification

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -1,18 +1,24 @@
 class SlackNotifier
-  def initialize(username = "WackRank", icon_emoji = ":table_tennis_paddle_and_ball:")
+  def initialize(icon_emoji = ":table_tennis_paddle_and_ball:")
     @webhook_url = ENV["SLACK_WEBHOOK_URL"]
-    @username = username
     @icon_emoji = icon_emoji
   end
 
   def send(text)
+    puts "About to create slack message"
+    puts @webhook_url
     return unless @webhook_url
+    
+
 
     headers = { 'Content-Type' => 'application/json' }
-    body = { "text": text, "icon_emoji": @icon_emoji, username: @username}
+    body = { "text": text, "icon_emoji": @icon_emoji}
 
     begin
       r = HTTParty.post(@webhook_url, body: body.to_json, headers: headers )
+      puts r.code
+      puts "Created slack message"
+
       return (r.code == 200)
     rescue
       return false
